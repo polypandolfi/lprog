e0d9cd1acb8bba4059a9ca50cee5b6795e34b627
polypandolfi <111456620+polypandolfi@users.noreply.github.com>
2022-08-17 19:12:44 +0000

    desenv.

--- 2/main.cs
+++ 2/main.cs
@@ -2,7 +2,39 @@ using System;
 
 class Aula2 {
 
+public struct Aluno {
+  public string matricula;
+  public int periodo;
+}
+
+public struct Disciplina {
+  public string nome;
+  public int semestre;
+}
+
+public static bool Matricular(Aluno aluno,
+                             Disciplina disciplina){
+return aluno.periodo == disciplina.semestre;  
+}
+  
 public static void Main (string [] args) {
+  
+  Aluno aluno1 = new Aluno {
+    matricula = "012022",
+      periodo = 1
+  };
+  
+  Aluno aluno2 = new Aluno {
+    matricula = "022022",
+      periodo = 3
+  };
+  
+  Disciplina disciplina = new Disciplina {
+    nome = "Linguagem de Programação",
+    semestre = 3
+  };
+  
   }
+  
   
 }
