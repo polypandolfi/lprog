c8feabd8bdb629f96b99a04bd64789a0507605ba
polypandolfi <111456620+polypandolfi@users.noreply.github.com>
2022-08-24 19:15:16 +0000

    2

---
+++ 3/aula3.cs
@@ -0,0 +1,30 @@
+using System;
+using Modelos;
+
+class Aula3 {
+  public static void print(Aluno aluno) {
+    Console.WriteLine(
+      ".....................\n" +
+      "Matricula: " + aluno.matricula + "\n" +
+      "Nome: " + aluno.nome + "\n" +
+      "Periodo: " + aluno.periodo + "\n" +
+      "....................."
+    );
+  }
+  public static void Main (string[] args) {
+    Aluno aluno1 = new Aluno {
+      matricula = "2022ENEL123" ,
+      nome = "Maria",
+      periodo = 0
+    };
+    print(aluno1);
+    Aluno aluno2 = new Aluno();
+    print(aluno2);
+    Aluno aluno3 = new Aluno(2);
+    print(aluno3);
+    Aluno aluno4 = new Aluno("2022ENEL456", "José");
+    print(aluno4);
+    Aluno aluno5 = new Aluno("2022ENEL789", "João", 3);
+    print(aluno5);
+  }
+}
--- modelos/Aluno.cs
+++ modelos/Aluno.cs
@@ -5,10 +5,31 @@ class Aluno {
   public string nome;
   public int periodo;
 
+  public Aluno() : this(0) {
+    
+  }
+  public Aluno(int periodo) {
+    if (periodo > 0) {
+      this.periodo = periodo;
+    } else {
+      this.periodo = 1;
+    }
+  }
+
+  public Aluno(string matricula, string nome) : this() {
+    this.matricula = matricula;
+    this.nome = nome;
+    }
+
+  public Aluno(string matricula, string nome, int periodo)
+                : this(periodo) {
+    this.matricula = matricula;
+    this.nome = nome;
+    }
   public bool Matricular(Disciplina disciplina) {
     return this.periodo == disciplina.semestre;
   }
     
 }
-  
 }
+  
